243626d4bc0f1a6f2dfa837eeb91bd7ce28a77fc
ChronoBlasted <84562359+AlexisGelin@users.noreply.github.com>
2023-04-14 17:04:02 +0200

    create list powerup

--- Assets/PowerUpManager.cs
+++ Assets/PowerUpManager.cs
@@ -1,8 +1,14 @@
+using BaseTemplate.Behaviours;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUpManager : MonoBehaviour
+public class PowerUpManager : MonoSingleton<PowerUpManager>
 {
     public List<PowerUp> powerUpList;
+
+    public PowerUp GetRandomPowerUp()
+    {
+        return powerUpList[Random.Range(0, powerUpList.Count)];
+    }
 }
